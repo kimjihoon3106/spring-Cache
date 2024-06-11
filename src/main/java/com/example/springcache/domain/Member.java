e2bdaab17111ff63463e7e25c32aa3df74f7e9e4
김지훈 <s24021@gsm.hs.kr>
2024-06-11 21:03:50 +0900

    Feat : "Domain 구현"

---
+++ src/main/java/com/example/springcache/domain/Member.java
@@ -0,0 +1,23 @@
+package com.example.springcache.domain;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Member {
+
+    private Long id;
+    private String name;
+    private Integer age;
+
+    public Member(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
